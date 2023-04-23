97f96112cea72e66992a9127fbfa002eff66ecf9
Yuniru Yuni <yuniruyuni@gmail.com>
2023-04-23 17:00:31 +0900

    refactor: delete already resolved TODO

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ async fn main() -> Result<()> {
     let channel = env::var("TWITCH_CHANNEL")
         .into_diagnostic()
         .wrap_err("TWITCH_CHANNEL must be set.")?;
-    // TODO: rename envvar name.
     let token = env::var("TWITCH_OAUTH_TOKEN")
         .into_diagnostic()
         .wrap_err("TWITCH_OAUTH_TOKEN must be set.")?;
