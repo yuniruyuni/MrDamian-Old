37e2cef9318ce7e4fb53c49acc2111cdf9da338b
Yuniru Yuni <yuniruyuni@gmail.com>
2023-05-08 22:38:06 +0900

    refactor: split menu construction into method

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,8 +8,8 @@ use miette::{IntoDiagnostic, Result, WrapErr};
 use std::env;
 
 use tauri::{
-    async_runtime, generate_context, generate_handler, Builder, CustomMenuItem, Manager, RunEvent,
-    SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+    async_runtime, generate_context, generate_handler, AppHandle, Builder, CustomMenuItem, Manager,
+    SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem, WindowEvent,
 };
 
 struct Config {
@@ -44,56 +44,81 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() -> Result<()> {
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(hide)
+enum SystemTrayMenuMode {
+    Hide,
+    Open,
+}
+
+fn create_tray_menu(mode: SystemTrayMenuMode) -> SystemTrayMenu {
+    let visibility = match mode {
+        SystemTrayMenuMode::Hide => CustomMenuItem::new("open".to_string(), "Open"),
+        SystemTrayMenuMode::Open => CustomMenuItem::new("hide".to_string(), "Hide"),
+    };
+
+    SystemTrayMenu::new()
+        .add_item(visibility)
         .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(quit);
-    let system_tray = SystemTray::new().with_menu(tray_menu);
+        .add_item(CustomMenuItem::new("quit".to_string(), "Quit"))
+}
+
+fn on_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::DoubleClick { .. } => {
+            let window = app.get_window("main").unwrap();
+            if window.is_visible().unwrap_or(false) {
+                window.hide().unwrap();
+                app.tray_handle()
+                    .set_menu(create_tray_menu(SystemTrayMenuMode::Hide))
+                    .expect("failed to set menu");
+            } else {
+                window.show().unwrap();
+                app.tray_handle()
+                    .set_menu(create_tray_menu(SystemTrayMenuMode::Open))
+                    .expect("failed to set menu");
+            }
+        }
+        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "hide" => {
+                let window = app.get_window("main").unwrap();
+                window.hide().unwrap();
+                app.tray_handle()
+                    .set_menu(create_tray_menu(SystemTrayMenuMode::Hide))
+                    .expect("failed to set menu");
+            }
+            "open" => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+                app.tray_handle()
+                    .set_menu(create_tray_menu(SystemTrayMenuMode::Open))
+                    .expect("failed to set menu");
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+}
+
+fn main() -> Result<()> {
+    let system_tray = SystemTray::new().with_menu(create_tray_menu(SystemTrayMenuMode::Hide));
+
+    async_runtime::spawn(async move {
+        let config = Config::load_envs()?;
+        let mut wsclient = Client::new(&config.bot, &config.channel, &config.token).await?;
+        wsclient.run().await
+    });
+
     Builder::default()
         .invoke_handler(generate_handler![greet])
         .system_tray(system_tray)
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::DoubleClick { .. } => {
-                let window = app.get_window("main").unwrap();
-                if window.is_visible().unwrap_or(false) {
-                    window.hide().unwrap();
-                } else {
-                    window.show().unwrap();
-                }
-            }
-            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                "quit" => {
-                    std::process::exit(0);
-                }
-                "hide" => {
-                    let window = app.get_window("main").unwrap();
-                    if window.is_visible().unwrap_or(false) {
-                        window.hide().unwrap();
-                    } else {
-                        window.show().unwrap();
-                    }
-                }
-                _ => {}
-            },
-            _ => {}
-        })
-        .setup(|_app| {
-            async_runtime::spawn(async move {
-                let config = Config::load_envs()?;
-                let mut wsclient = Client::new(&config.bot, &config.channel, &config.token).await?;
-                wsclient.run().await
-            });
-            Ok(())
-        })
-        .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::CloseRequested { api, .. } => {
+        .on_system_tray_event(on_system_tray_event)
+        .on_window_event(|event| {
+            if let WindowEvent::CloseRequested { api, .. } = event.event() {
                 event.window().hide().expect("failed to hide window");
                 api.prevent_close();
             }
-            _ => (),
         })
         .run(generate_context!())
         .into_diagnostic()
