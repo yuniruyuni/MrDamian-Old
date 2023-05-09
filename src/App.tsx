af4d9cbc3e9c773856b386f8110e6fcf84e525d7
Yuniru Yuni <yuniruyuni@gmail.com>
2023-05-09 23:09:56 +0900

    feat: allow to remove edges in node editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useMemo, useEffect } from "react";
+import { useCallback, useMemo, useEffect, useRef } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 
-import type { Node } from 'reactflow';
+import type { Node, Edge, Connection, HandleType } from 'reactflow';
 
 import ReactFlow, {
   MiniMap,
@@ -9,7 +9,8 @@ import ReactFlow, {
   Background,
   useNodesState,
   useEdgesState,
-  addEdge
+  addEdge,
+  updateEdge
 } from 'reactflow';
 
 import 'reactflow/dist/style.css';
@@ -19,9 +20,25 @@ import { PropertiesNode } from "./PropertiesNode";
 import type { NodeData } from "./pipeline";
 
 function App() {
+  const edgeUpdateSuccessful = useRef(true);
+
   const nodeTypes = useMemo(() => ({ propertiesNode: PropertiesNode, }), []);
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const onEdgeUpdateStart = useCallback(() => {
+    edgeUpdateSuccessful.current = false;
+  }, []);
+  const onEdgeUpdate = useCallback((oldEdge: Edge, newConnection: Connection) => {
+    edgeUpdateSuccessful.current = true;
+    setEdges((edges) => updateEdge(oldEdge, newConnection, edges));
+  }, []);
+  const onEdgeUpdateEnd = useCallback((_event: MouseEvent | TouchEvent, target: Edge, _handle: HandleType) => {
+    if(!edgeUpdateSuccessful.current) {
+      setEdges((edges) => edges.filter((e) => e.id !== target.id));
+    }
+    edgeUpdateSuccessful.current = true;
+  }, []);
 
   const onConnect = useCallback(
     (params: any) => setEdges((edge) => addEdge(params, edge)),
@@ -49,6 +66,9 @@ function App() {
         nodeTypes={nodeTypes}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        onEdgeUpdateStart={onEdgeUpdateStart}
+        onEdgeUpdate={onEdgeUpdate}
+        onEdgeUpdateEnd={onEdgeUpdateEnd}
         onConnect={onConnect}
       >
         <MiniMap />
