c3a1668e5a4cc7b7682a88bb5b550713ff9f9278
Yuniru Yuni <yuniruyuni@gmail.com>
2023-05-09 23:18:22 +0900

    refactor: split usePipeline hook from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import { useCallback, useMemo, useEffect, useRef } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-
-import type { Node, Edge, Connection, HandleType } from 'reactflow';
-
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -15,62 +10,13 @@ import ReactFlow, {
 
 import 'reactflow/dist/style.css';
 
-import { PropertiesNode } from "./PropertiesNode";
-
-import type { NodeData } from "./pipeline";
+import { usePipeline } from "./pipeline";
 
 function App() {
-  const edgeUpdateSuccessful = useRef(true);
-
-  const nodeTypes = useMemo(() => ({ propertiesNode: PropertiesNode, }), []);
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-
-  const onEdgeUpdateStart = useCallback(() => {
-    edgeUpdateSuccessful.current = false;
-  }, []);
-  const onEdgeUpdate = useCallback((oldEdge: Edge, newConnection: Connection) => {
-    edgeUpdateSuccessful.current = true;
-    setEdges((edges) => updateEdge(oldEdge, newConnection, edges));
-  }, []);
-  const onEdgeUpdateEnd = useCallback((_event: MouseEvent | TouchEvent, target: Edge, _handle: HandleType) => {
-    if(!edgeUpdateSuccessful.current) {
-      setEdges((edges) => edges.filter((e) => e.id !== target.id));
-    }
-    edgeUpdateSuccessful.current = true;
-  }, []);
-
-  const onConnect = useCallback(
-    (params: any) => setEdges((edge) => addEdge(params, edge)),
-    [setEdges],
-  );
-
-  useEffect(() => {
-    (async () => {
-      const response = await invoke<{id: string, data: NodeData}[]>("nodes");
-      const nodes: Node[] = response.map(({id, data}) => ({
-        type: "propertiesNode",
-        id,
-        data,
-        position: { x: 0, y: 0 },
-      }));
-      setNodes(nodes);
-    })()
-  }, []);
-
+  const pipeline = usePipeline();
   return (
     <div className="container">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        nodeTypes={nodeTypes}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onEdgeUpdateStart={onEdgeUpdateStart}
-        onEdgeUpdate={onEdgeUpdate}
-        onEdgeUpdateEnd={onEdgeUpdateEnd}
-        onConnect={onConnect}
-      >
+      <ReactFlow {...pipeline} >
         <MiniMap />
         <Controls />
         <Background />
--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,3 +1,17 @@
+import { useCallback, useMemo, useEffect, useRef } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+
+import type { Node, Edge, Connection, HandleType } from 'reactflow';
+
+import {
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  updateEdge
+} from 'reactflow';
+
+import { PropertiesNode } from "./PropertiesNode";
+
 export type InputPort = {
   name: string,
 };
@@ -11,3 +25,55 @@ export type NodeData = {
   inputs: InputPort[],
   outputs: OutputPort[],
 };
+
+export function usePipeline() {
+  const edgeUpdateSuccessful = useRef(true);
+
+  const nodeTypes = useMemo(() => ({ propertiesNode: PropertiesNode, }), []);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const onEdgeUpdateStart = useCallback(() => {
+    edgeUpdateSuccessful.current = false;
+  }, []);
+  const onEdgeUpdate = useCallback((oldEdge: Edge, newConnection: Connection) => {
+    edgeUpdateSuccessful.current = true;
+    setEdges((edges) => updateEdge(oldEdge, newConnection, edges));
+  }, []);
+  const onEdgeUpdateEnd = useCallback((_event: MouseEvent | TouchEvent, target: Edge, _handle: HandleType) => {
+    if(!edgeUpdateSuccessful.current) {
+      setEdges((edges) => edges.filter((e) => e.id !== target.id));
+    }
+    edgeUpdateSuccessful.current = true;
+  }, []);
+
+  const onConnect = useCallback(
+    (connection: Connection) => setEdges((edge) => addEdge(connection, edge)),
+    [setEdges],
+  );
+
+  useEffect(() => {
+    (async () => {
+      const response = await invoke<{id: string, data: NodeData}[]>("nodes");
+      const nodes: Node[] = response.map(({id, data}) => ({
+        type: "propertiesNode",
+        id,
+        data,
+        position: { x: 0, y: 0 },
+      }));
+      setNodes(nodes);
+    })()
+  }, []);
+
+  return {
+    nodes,
+    edges,
+    nodeTypes,
+    onNodesChange,
+    onEdgesChange,
+    onEdgeUpdateStart,
+    onEdgeUpdate,
+    onEdgeUpdateEnd,
+    onConnect,
+  };
+}
