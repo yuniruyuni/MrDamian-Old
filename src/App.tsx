981184aee2e535892e15370fbe24e4c863cf9040
Yuniru Yuni <yuniruyuni@gmail.com>
2023-05-13 17:15:33 +0900

    feat: add dummy context menu implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useMemo } from 'react';
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -6,20 +7,47 @@ import ReactFlow, {
 
 import 'reactflow/dist/style.css';
 
-import { Button } from 'semantic-ui-react';
+import { Button, Dropdown, Portal, Segment } from 'semantic-ui-react'
 
 import { usePipeline } from "./pipeline";
 
+type Menu = {
+  open: boolean;
+  x: number;
+  y: number;
+};
+
 function App() {
   const { onApply, ...pipeline } = usePipeline();
+
+  const [ menu, setMenu ] = useState<Menu>({open: false, x: 0, y: 0});
+
+  type Component = {name: string};
+
+  const components: Component[] = useMemo(() => [
+    {name: 'TwitchSubscriber'},
+    {name: 'TwitchPublisher'},
+  ], []);
+
   return (
     <div className="container">
-      <ReactFlow {...pipeline} >
+      <ReactFlow
+        {...pipeline}
+        onPaneContextMenu={ (e) => { setMenu({open: true, x: e.clientX, y: e.clientY}); } }
+      >
         <MiniMap />
         <Controls />
         <Background />
       </ReactFlow>
       <Button onClick={onApply} primary>Apply</Button>
+      <Portal open={menu.open} onClose={() => { setMenu({open: false, x: 0, y: 0}) }}>
+        {/* TODO: avoid ad-hock mouse position fixing. */}
+        <Segment style={{position: 'absolute', left: menu.x - 8, top: menu.y - 16}}>
+          <Dropdown.Menu visible={menu.open}>
+            {components.map(({name}) => (<Dropdown.Item>{name}</Dropdown.Item>))}
+          </Dropdown.Menu>
+        </Segment>
+      </Portal>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import "./styles.css";
 import 'semantic-ui-css/semantic.min.css';
 
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
